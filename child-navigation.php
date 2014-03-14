754d213a42183284972e626ac83e1e05bfe408de
hildende <hildende@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-14 15:46:13 +0000

    Added support for start level Added possibility to show current page
    git-svn-id: https://plugins.svn.wordpress.org/child-navigation/trunk@875492 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- child-navigation.php
+++ child-navigation.php
@@ -2,41 +2,155 @@
 /*
  * Plugin Name: Child Navigation 
  * Description: Adds support for child navigations to wp_nav_menu()
- * Version: 1.0
+ * Version: 1.1
  * Author: Dennis Hildenbrand
  */
 
 
 class Child_Navigation {
 
-	public function filter_submenu( $items, $args ) {
-		if ( ! $args->children_only ) {
-			return $items;
-		}
+	protected $items;
+	protected $args;
+	protected $itemStorage = array();
+	protected $filterCall = 0;
+	protected $cssCall = 0;
 
-		$currentMenuItem = array_shift ( wp_filter_object_list ( $items, array( 'current' => true ) ) );
-		$children = $this->find_sub_items( $currentMenuItem->ID, $items, $args->depth );
+	protected function init($items, $args){
+		$this->items = $items;
+		$this->args = $args;
+		$this->args->depth = $this->args->depth ? $this->args->depth : 999;
 
-		foreach ( $items as $key => $item ) {
-			if ( ! in_array( $item->ID, $children ) ) {
-				unset( $items[$key] );
-			}
-
-		}
-		return $items;
 	}
 
-	protected function find_sub_items( $id, $items, $maxDepth = 999, $depth = 0 ) {
+	public function filter_submenu($items, $args) {
+		$this->filterCall++;
+		$this->init($items, $args);
+
+		if (!$this->args->children_only)
+			return $this->items;
+
+		$startItem = $this->getStartItem();
+
+		if(!$startItem)
+			return array();
+
+
+		$depth = $this->args->depth;
+		if($this->args->children_show_start_level) {
+			$depth--;
+		}
+		$children = array();
+		if($depth) {
+			$children = $this->find_sub_items( $startItem->ID, $depth);
+		}
+
+		foreach ( $this->items as $key => $item ) {
+			if ( ! in_array( $item->ID, $children ) ) {
+				unset( $this->items[$key] );
+			}
+		}
+
+		if($this->args->children_show_start_level) {
+			$startItem->menu_item_parent = '';
+			$this->items[] = $startItem;
+		}
+
+		$this->itemStorage[$this->filterCall] = $this->items;
+		return $this->items;
+	}
+
+	protected function find_sub_items( $id, $maxDepth = 999, $depth = 0 ) {
 		$depth ++;
-		$ids = wp_filter_object_list( $items, array( 'menu_item_parent' => $id ), 'and', 'ID' );
+		$ids = wp_filter_object_list($this->items, array( 'menu_item_parent' => $id ), 'and', 'ID');
 		if( $depth < $maxDepth ) {
 			foreach ( $ids as $id ) {
-				$ids = array_merge( $ids, $this->find_sub_items( $id, $items, $maxDepth, $depth ) );
+				$ids = array_merge( $ids, $this->find_sub_items( $id, $this->items, $maxDepth, $depth ) );
 			}
 		}
 		return $ids;
 	}
 
+	protected function getCurrentDepth($items, $current, $depth = 0) {
+		$depth++;
+		$found = false;
+		foreach($items as $item) {
+			if($item->ID == $current->menu_item_parent) {
+				$found = true;
+				$current = $item;
+				break;
+			}
+		}
+		if($found)
+			return $this->getCurrentDepth($items, $current, $depth);
+		return $depth;
+	}
+
+	protected function getStartItem(){
+		$startItem = array_shift ( wp_filter_object_list ( $this->items, array( 'current' => true ) ) );
+
+		if(!$this->args->children_start_level)
+			return $startItem;
+
+		$currentDepth = $this->getCurrentDepth($this->items, $startItem);
+		$steps = $currentDepth - $this->args->children_start_level;
+		$startItem = $this->goBackInRootline($startItem, $steps);
+
+		return $startItem;
+	}
+
+	protected function goBackInRootline($startItem, $steps) {
+		if($steps < 0)
+			return null;
+
+		while($steps) {
+			$startItem = array_shift(wp_filter_object_list($this->items, array('ID' => $startItem->menu_item_parent)));
+			$steps--;
+		}
+
+		return $startItem;
+	}
+
+	public function cleanCssClasses($items, $args) {
+		$this->cssCall++;
+		$this->init($items, $args);
+		$itemStorage = $this->itemStorage[$this->cssCall];
+
+		if(!$itemStorage)
+			return $this->items;
+
+		$pattern = '(?<=class=").*?(menu-item).*?(?=")';
+		preg_match_all('#' . $pattern . '#is', $this->items, $matches);
+		$menuItemClasses = $matches[0];
+
+		foreach($itemStorage as $item) {
+			$this->checkSingleItemForCss($item->ID, $itemStorage, $menuItemClasses);
+		}
+
+		return $this->items;
+	}
+
+	protected function checkSingleItemForCss($itemID, $items, $menuItemClasses){
+		if($this->hasChildren($itemID, $items))
+			return;
+		foreach($menuItemClasses as $menuItem) {
+			if(strpos($menuItem, 'menu-item-' . $itemID) !== false) {
+
+				$tmpClasses = str_replace('menu-item-has-children', '', $menuItem);
+				$this->items = str_replace($menuItem, $tmpClasses, $this->items);
+			}
+		}
+	}
+
+	protected function hasChildren($itemID, $items){
+		foreach($items as $item) {
+			if($item->menu_item_parent == $itemID)
+				return true;
+		}
+		return false;
+	}
+
 }
 
-add_filter( 'wp_nav_menu_objects', array( new Child_Navigation() ,'filter_submenu' ), 10, 2 );
+$child_navigation = new Child_Navigation();
+add_filter( 'wp_nav_menu_objects', array( $child_navigation,'filter_submenu' ), 10, 2 );
+add_filter( 'wp_nav_menu_items', array( $child_navigation ,'cleanCssClasses' ), 10, 2 );
--- readme.txt
+++ readme.txt
@@ -11,7 +11,7 @@ Adds support for child navigation to `wp_nav_menu()`
 
 == Description ==
 
-This plugin adds support for displaying only child pages of the current page to wp_nav_menu()
+This plugin adds support for displaying child pages of the current page to wp_nav_menu()
 
 == Installation ==
 1. Upload `child-navigation` directory to the `/wp-content/plugins/` directory
@@ -22,4 +22,19 @@ Set `children_only` as argument to `TRUE` in `wp_nav_menu()` to display only chi
 
 `wp_nav_menu( array ( 'children_only' => TRUE ) );`
 
-the argument `depth` will be relative to the first child level
+Set `children_start_level` as argument with the wanted start level as value to show pages in the current root line starting with given level.
+
+`wp_nav_menu( array ( 'children_start_level' => 2 ) );`
+
+Set `children_show_start_level` as argument to `TRUE` to show the start level (or the current page). if this is not set only child pages are shown.
+
+`wp_nav_menu( array ( 'children_show_start_level' => TRUE ) );`
+
+The argument `depth` will be relative to the first displayed level.
+
+
+== Changelog ==
+
+= 1.1.0 =
+* Added support for start level
+* Added possibility to show current page
